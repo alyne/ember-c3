388691804ad4c090ccc9272632e226e8798e8a86
Khadim Fall <khadim.fall@alyne.com>
2018-08-15 11:03:21 +0200

    added c3-shim again

--- blueprints/ember-c3/index.js
+++ blueprints/ember-c3/index.js
@@ -2,7 +2,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {}
-  // afterInstall: function() {
-  //   return this.addAddonToProject('ember-c3-shim@0.0.6');
-  // }
+  afterInstall: function() {
+    return this.addAddonToProject('ember-c3-shim@0.0.6');
+  }
 };
