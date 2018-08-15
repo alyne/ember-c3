f1650ae4c46fe414aafb1d9eda619b9a154cc943
Khadim Fall <khadim.fall@alyne.com>
2018-08-15 11:17:18 +0200

    removed doubled line

--- blueprints/ember-c3/index.js
+++ blueprints/ember-c3/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   normalizeEntityName: function() {},
 
-  afterInstall: function() {}
   afterInstall: function() {
     return this.addAddonToProject('ember-c3-shim@0.0.6');
   }
